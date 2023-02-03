4611d5a28b705a04ea95dd04f8534be4a0a6e559
yamadanaofumi <17900178+na0fu3y@users.noreply.github.com>
2023-02-04 01:03:01 +0900

    Dataform実行用のスケジューラを、データ収集パイプラインに1本化

--- terraform/modules/lineage/main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project_service" "lineage" {
-  for_each = toset([
-    "datalineage.googleapis.com",
-  ])
-
-  project            = var.project_id
-  service            = each.key
-  disable_on_destroy = false
-}
