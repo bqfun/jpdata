bad0488ea82616f69a89e231ee534408ba7085e2
Naofumi Yamada <17900178+na0fu3y@users.noreply.github.com>
2024-08-18 23:00:29 +0900

    Update main.tf

--- terraform_snowflake/modules/common/main.tf
+++ terraform_snowflake/modules/common/main.tf
@@ -9,6 +9,13 @@ resource "snowflake_task" "main" {
     SELECT table_catalog, table_schema, table_name FROM ${var.podb_database}.information_schema.tables WHERE table_schema = '${var.podb_schema}';
 
   BEGIN
+    EXECUTE IMMEDIATE '
+      COPY INTO @"${var.stage_database}"."${var.stage_schema}"."${var.stage_name}"/INFORMATION_SCHEMA/${var.podb_database}/${var.podb_schema}/
+      FROM (SELECT table_catalog, table_schema, table_name, comment FROM ${var.podb_database}.information_schema.tables WHERE table_schema = \'${var.podb_schema}\')
+      FILE_FORMAT = (TYPE = PARQUET)
+      HEADER = TRUE
+      OVERWRITE = TRUE;
+    ';
     FOR record IN cursor DO
       EXECUTE IMMEDIATE '
         COPY INTO @"${var.stage_database}"."${var.stage_schema}"."${var.stage_name}"/' || record.table_catalog || '/' || record.table_schema || '/' || record.table_name || '/'
