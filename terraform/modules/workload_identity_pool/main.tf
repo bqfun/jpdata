9c288d32fa019f33f4b2c75b6baf22bb4c21e514
Naofumi Yamada <17900178+na0fu3y@users.noreply.github.com>
2024-08-19 23:15:02 +0900

    plan / apply の pool を分離

--- terraform/modules/workload_identity_pool/main.tf
+++ terraform/modules/workload_identity_pool/main.tf
@@ -34,21 +34,47 @@ resource "google_project_iam_member" "terraform_apply" {
   member  = "serviceAccount:${google_service_account.terraform_apply.email}"
 }
 
-resource "google_iam_workload_identity_pool" "main" {
-  workload_identity_pool_id = "github-actions"
+resource "google_iam_workload_identity_pool" "plan" {
+  workload_identity_pool_id = "github-actions-terraform-plan"
   depends_on = [
     google_project_service.main
   ]
 }
 
-resource "google_iam_workload_identity_pool_provider" "main" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.main.workload_identity_pool_id
-  workload_identity_pool_provider_id = "main"
+resource "google_iam_workload_identity_pool" "apply" {
+  workload_identity_pool_id = "github-actions-terraform-apply"
+  depends_on = [
+    google_project_service.main
+  ]
+}
+
+resource "google_iam_workload_identity_pool_provider" "plan" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.plan.workload_identity_pool_id
+  workload_identity_pool_provider_id = "plan"
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
-    "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
+    "attribute.ref"        = "assertion.ref"
+    "attribute.event_name" = "assertion.event_name"
+    "attribute.base_ref"   = "assertion.base_ref"
   }
+  attribute_condition = "attribute.repository == \"bqfun/jpdata\" && attribute.base_ref == \"main\""
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_iam_workload_identity_pool_provider" "apply" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.apply.workload_identity_pool_id
+  workload_identity_pool_provider_id = "apply"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.ref"        = "assertion.ref"
+    "attribute.event_name" = "assertion.event_name"
+    "attribute.base_ref"   = "assertion.base_ref"
+  }
+  attribute_condition = "attribute.repository == \"bqfun/jpdata\" && attribute.event_name == \"push\" && attribute.ref == \"refs/heads/main\""
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
@@ -57,11 +83,11 @@ resource "google_iam_workload_identity_pool_provider" "main" {
 resource "google_service_account_iam_member" "terraform_plan" {
   service_account_id = google_service_account.terraform_plan.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.main.name}/attribute.repository/bqfun/jpdata"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.plan.name}/attribute.repository/bqfun/jpdata"
 }
 
 resource "google_service_account_iam_member" "terraform_apply" {
   service_account_id = google_service_account.terraform_apply.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.main.name}/attribute.repository/bqfun/jpdata"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.apply.name}/attribute.repository/bqfun/jpdata"
 }
